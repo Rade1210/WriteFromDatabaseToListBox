99d3bdd31d9511398c0be4dd38d4fea6da8d1f39
Rade1210 <39063708+Rade1210@users.noreply.github.com>
2020-11-15 11:54:18 +0100

    Beginning of the project, created project with database

---
+++ Klasican ispis iz baze u listu/Klasican ispis iz baze u listu/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Klasican_ispis_iz_baze_u_listu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
